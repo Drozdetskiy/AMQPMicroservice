5dbff8f46a1008c3775b5a08c47f063b63894cef
Drozdetskiy <m.drozdetskiy@gmail.com>
2019-07-25 10:34:55 +0300

    Rename concumer to producer. Yep, it was a fail(

--- producer/producer_mock.py
+++ producer/producer_mock.py
@@ -56,7 +56,6 @@ def generate_message():
     return 'test message'
 
 
-# Need to rename file from consumer to producer!
 if __name__ == '__main__':
     rabbit_credentials = RabbitCredsContainer()
 
