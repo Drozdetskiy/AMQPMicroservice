8370d279e1600284070cd0a28d9e02d070ae8d66
Drozdetskiy <m.drozdetskiy@gmail.com>
2019-08-08 00:53:37 +0300

    create UserRating Materialized view and update log model

--- amqpservice/rating_api/models.py
+++ amqpservice/rating_api/models.py
@@ -10,3 +10,32 @@ class UserRating(models.Model):
         return f'UserRating user_id: {self.user_id} - ' \
                f'rating: {self.rating} - ' \
                f'datetime: {self.datetime}'
+
+    def __str__(self):
+        return f'UserRating user_id: {self.user_id} - ' \
+               f'rating: {self.rating} - ' \
+               f'datetime: {self.datetime}'
+
+
+class RefreshLog(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    duration = models.DurationField()
+
+
+class UserRatingView(models.Model):
+    user_id = models.IntegerField(unique=True)
+    rating = models.FloatField()
+    datetime = models.DateTimeField()
+
+    class Meta:
+        managed = False
+
+    def __repr__(self):
+        return f'UserRating user_id: {self.user_id} - ' \
+               f'rating: {self.rating} - ' \
+               f'datetime: {self.datetime}'
+
+    def __str__(self):
+        return f'UserRating user_id: {self.user_id} - ' \
+               f'rating: {self.rating} - ' \
+               f'datetime: {self.datetime}'
