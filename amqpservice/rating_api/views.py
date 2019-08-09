dd303be80fe8c7cf05359417389bdf866440cb99
Drozdetskiy <m.drozdetskiy@gmail.com>
2019-08-10 00:20:13 +0300

    Add test view for testing celery task and celery backend

--- amqpservice/rating_api/views.py
+++ amqpservice/rating_api/views.py
@@ -1,3 +1,16 @@
+import redis
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class TestView(APIView):
+    def get(self, request):
+        r = redis.Redis(host='redis', port=6379, db=2)
+        keys = r.keys('*')
+        res = []
+        for i in keys:
+            res.append(r.get(i))
+        return Response(res)
