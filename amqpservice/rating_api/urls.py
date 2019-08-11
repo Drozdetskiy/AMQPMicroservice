21b0dff7966c4bb8dfed769fe95e7dbd7008c719
Drozdetskiy <m.drozdetskiy@gmail.com>
2019-08-11 21:23:49 +0300

    Add detail view for rating. Start to create LeaderBoardView

--- amqpservice/rating_api/urls.py
+++ amqpservice/rating_api/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from rating_api.views import TestView
+from rating_api.views import TestView, LeaderBoardView, DetailRatingView
 
 urlpatterns = [
     path('', TestView.as_view(), name='test_view'),
+    re_path(
+        r'leader_board/(?P<classifier>gt|lt)',
+        LeaderBoardView.as_view(),
+        name='leader_board_view'
+    ),
+    path(
+        'leader_board/<int:position>',
+        DetailRatingView.as_view(),
+        name='detail_rating_view'
+    ),
 ]
--- amqpservice/rating_api/views.py
+++ amqpservice/rating_api/views.py
@@ -1,19 +1,54 @@
-import redis
-from django.conf import settings
-from django.shortcuts import render
+import pickle
 
-# Create your views here.
+import redis
+
+from django.conf import settings
+from django.utils.datastructures import MultiValueDictKeyError
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from rating_api.models import UserRatingView
+
+class RedisConnectionMixin:
+    connection = redis.Redis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        db=settings.REDIS_BOARD_DB
+    )
 
 
-class TestView(APIView):
+# Create your views here.
+class TestView(APIView, RedisConnectionMixin):
     def get(self, request):
-        r = redis.Redis(host='redis', port=6379, db=settings.REDIS_BOARD_DB)
-        cache_key = r.get('cache_key')
-        res = r.zrange(cache_key, 0, 5, withscores=True)
-        print(res, r.keys('*'))
-        r = redis.StrictRedis(host='redis', port=6379, db=1)
+        cache_key = self.connection.get('cache_key')
+        res = self.connection.zrange(cache_key, 0, 0, withscores=True)
+        print(res, self.connection.keys('*'))
         return Response('ok')
+
+
+class LeaderBoardView(APIView, RedisConnectionMixin):
+    def get(self, request, classifier):
+        try:
+            position = request.GET['position']
+            page_len = request.GET['page_len']
+            page = request.GET['page']
+        except MultiValueDictKeyError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response((classifier, position, page_len, page))
+
+
+class DetailRatingView(APIView, RedisConnectionMixin):
+    def get(self, request, position):
+        cache_key = self.connection.get('cache_key')
+        if not position:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        raw_res = self.connection.zrange(
+            cache_key,
+            position - 2 if position > 1 else 0,
+            position,
+        )
+        res = map(pickle.loads, raw_res)
+
+        return Response(res)
